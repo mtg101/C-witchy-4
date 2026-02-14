ccee09ed0cfba54b839da480342d68fd9608db5d
Russell Bulmer <russell.bulmer@gmail.com>
2026-02-14 22:33:44 +0000

    first commit

---
+++ src/main.asm
@@ -0,0 +1,37 @@
+;
+; VS64 generated Example was the starting point...
+;
+
+*=$0801
+!byte $0c,$08,$b5,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+
+    jmp     MAIN
+
+!source     "src/c64_defs.asm"
+!source     "src/c64_maths.asm"
+!source     "src/c64_raster.asm"
+
+MAIN
+    jsr     ROM_CLR_SCREEN
+    jsr     MATHS_SETUP_RNG
+    jsr     RASTER_INTERRUPT_SETUP
+
+    ldy     #0
+
+HELLO
+    lda     .hellotext,y
+    beq     +
+    sta     SCREEN_RAM+15+SCREEN_WIDTH_CHARS,y
+    lda     #1
+    sta     $D800+15+SCREEN_WIDTH_CHARS,y
+    iny
+    jmp     HELLO
++
+
+HOLD
+    jmp     HOLD
+
+.hellotext
+    !scr    "c-witchy-4",0
+
+
